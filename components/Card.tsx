ba46a11164ca97b60b378254c38eb013f4ab5e3b
case <case@casesandberg.com>
2023-12-12 17:20:54 -0800

    Animate the Cards

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,7 +1,35 @@
-export function Card({ children, className }: { children?: React.ReactNode; className?: string }) {
+'use client'
+
+import { motion } from 'framer-motion'
+
+export function Card({
+  children,
+  className,
+  delay = 0,
+}: {
+  children?: React.ReactNode
+  className?: string
+  delay?: number
+}) {
   return (
-    <div className={`relative flex flex-col items-center justify-center rounded-3xl bg-foreground p-4 ${className}`}>
+    <motion.div
+      variants={{
+        hidden: { opacity: 0, scale: 0.75 },
+        show: {
+          opacity: 1,
+          scale: 1,
+          transition: {
+            type: 'spring',
+            stiffness: 100,
+            delay,
+          },
+        },
+      }}
+      initial="hidden"
+      animate="show"
+      className={`relative flex flex-col items-center justify-center rounded-3xl bg-foreground p-4 ${className}`}
+    >
       {children}
-    </div>
+    </motion.div>
   )
 }
