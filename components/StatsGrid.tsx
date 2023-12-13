b5fb394f3677934780e3a52e10855ab01152945c
case <case@casesandberg.com>
2023-12-12 23:27:05 -0800

    Add Loading on User Card

--- components/StatsGrid.tsx
+++ components/StatsGrid.tsx
@@ -60,10 +60,30 @@ const TerminalIcon = ({ className }: { className?: string }) => (
   </svg>
 )
 
-const WorkspacePremium = ({ className }: { className?: string }) => (
+const WorkspacePremiumIcon = ({ className }: { className?: string }) => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
     <path
       d="M9.68 13.19L12 11.43L14.31 13.19L13.43 10.34L15.75 8.5H12.91L12 5.69L11.09 8.5H8.25L10.56 10.34L9.68 13.19ZM20 9.5C20 5.08 16.42 1.5 12 1.5C7.58 1.5 4 5.08 4 9.5C4 11.53 4.76 13.37 6 14.78V22.5L12 20.5L18 22.5V14.78C19.24 13.37 20 11.53 20 9.5ZM12 3.5C15.31 3.5 18 6.19 18 9.5C18 12.81 15.31 15.5 12 15.5C8.69 15.5 6 12.81 6 9.5C6 6.19 8.69 3.5 12 3.5ZM12 18.5L8 19.52V16.42C9.18 17.1 10.54 17.5 12 17.5C13.46 17.5 14.82 17.1 16 16.42V19.52L12 18.5Z"
+      fill="currentColor"
+    />
+  </svg>
+)
+
+const LoadingIcon = () => (
+  <svg
+    aria-hidden="true"
+    role="status"
+    className="inline h-6 w-6 animate-spin text-white"
+    viewBox="0 0 100 101"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+      fill="rgba(0,0,0,0.2)"
+    />
+    <path
+      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
       fill="currentColor"
     />
   </svg>
@@ -124,25 +144,40 @@ function calculateDayWidth(
   return `${finalRatio * 100}%`
 }
 
-function UserCard({ user, className }: { user: User; className?: string }) {
+function UserCard({ user, loading, className }: { user: User; loading?: boolean; className?: string }) {
   return (
     <CardWithExternalLink
       href={`https://manifold.markets/${user.username}`}
-      className={`via-purple-500 col-span-2 bg-gradient-to-br from-indigo-500 to-pink-500 ${className}`}
+      className={
+        loading
+          ? `via-purple-500 col-span-2 bg-gradient-to-br from-indigo-500/20 to-pink-500/20 ${className}`
+          : `via-purple-500 col-span-2 bg-gradient-to-br from-indigo-500 to-pink-500 ${className}`
+      }
       linkClassName="bg-transparent"
     >
+      {loading ? (
+        <div className="absolute left-5 top-4">
+          <LoadingIcon />
+        </div>
+      ) : null}
       <div
         className="text-7xl font-medium tracking-tighter text-shadow"
         style={{ fontSize: calculateFontSize(user.username), lineHeight: 1, height: calculateFontSize(user.username) }}
       >
         <TypedName name={user.username}></TypedName>
       </div>
-      <div className="text-center text-foreground-light">2023 Year in Review</div>
+      {loading ? (
+        <div className="text-center text-foreground-light">Generating. Please wait a minute.</div>
+      ) : (
+        <div className="text-center text-foreground-light">2023 Year in Review</div>
+      )}
     </CardWithExternalLink>
   )
 }
 
 export default function StatsGrid({ user, stats }: { user: User; stats: Stats }) {
+  const isGenerating = stats.processing && Object.keys(stats.items).length === 0
+
   return (
     <div className="my-8 grid auto-rows-[200px] grid-cols-2 gap-3 md:grid-cols-4 lg:grid-cols-6">
       {stats.items.USER_STREAK ? (
@@ -169,7 +204,7 @@ export default function StatsGrid({ user, stats }: { user: User; stats: Stats })
         <Card className="md:hidden" opacity={0.5} delay={0.1} />
       )}
 
-      <UserCard user={user} className="md:flex lg:hidden" />
+      <UserCard user={user} loading={isGenerating} className="md:flex lg:hidden" />
 
       {stats.items.POSITIONS_LARGEST_PROFIT ? (
         <CardWithExternalLink
@@ -252,7 +287,7 @@ export default function StatsGrid({ user, stats }: { user: User; stats: Stats })
                     break
                   }
                   case 'masters': {
-                    award = <Award label="Masters" Icon={WorkspacePremium} className="bg-[#D425F9]" />
+                    award = <Award label="Masters" Icon={WorkspacePremiumIcon} className="bg-[#D425F9]" />
                     break
                   }
                   default: {
@@ -297,7 +332,7 @@ export default function StatsGrid({ user, stats }: { user: User; stats: Stats })
         <Card opacity={0.5} delay={0.5} />
       )}
 
-      <UserCard user={user} className="md:hidden lg:flex" />
+      <UserCard user={user} loading={isGenerating} className="md:hidden lg:flex" />
 
       {stats.items.POSITIONS_BEST_PERCENT_LOSS ? (
         <CardWithExternalLink href={stats.items.POSITIONS_BEST_PERCENT_LOSS.url} className="gap-4" delay={0.3}>
