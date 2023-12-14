7fa4080e8a0f4b6be2770ea34c017a19c18f08aa
case <case@casesandberg.com>
2023-12-14 14:49:45 -0800

    Adjust Mobile Styles on Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -16,16 +16,20 @@ export default function Home() {
   return (
     <Container>
       <div className="flex h-full flex-col items-center justify-center">
-        <h1 className="via-purple-500 mb-20 inline-block bg-gradient-to-l from-indigo-500 to-pink-500 bg-clip-text p-4 text-7xl font-semibold tracking-tighter text-transparent">
+        <h1 className="via-purple-500 mb-6 inline-block bg-gradient-to-l from-indigo-500 to-pink-500 bg-clip-text p-4 text-3xl font-semibold tracking-tighter text-transparent md:mb-20 md:text-7xl">
           Manifold Year in Review
         </h1>
 
         <div className="grid w-full auto-rows-[200px] grid-cols-2 gap-3 md:grid-cols-4 lg:grid-cols-6">
-          <Card className="lg:col-[2] lg:row-[1]">
+          <Card className="lg:col-[2] lg:row-[1]" delay={0.2}>
             <div className="flex flex-1 flex-col justify-center text-center text-5xl font-bold">43K</div>
             <div className="text-center text-foreground-light">New Users</div>
           </Card>
-          <CardWithExternalLink href="https://manifold.markets/charity" className="!bg-blue lg:col-[2] lg:row-[2]">
+          <CardWithExternalLink
+            href="https://manifold.markets/charity"
+            className="!bg-blue lg:col-[2] lg:row-[2]"
+            delay={0.1}
+          >
             <div className={`flex flex-1 flex-col justify-center font-mono text-3xl font-medium text-shadow`}>
               <FormatNumber value={123954} prefix="$" />
             </div>
@@ -46,6 +50,7 @@ export default function Home() {
           <CardWithExternalLink
             href="https://manifold.markets/QuantumObserver/will-the-lk99-room-temp-ambient-pre"
             className="lg:col-[5] lg:row-[1]"
+            delay={0.3}
           >
             <div className="flex flex-1 flex-col items-center justify-center gap-3 overflow-clip">
               <div>
@@ -54,9 +59,9 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="shrink-0 text-center text-foreground-light">Most popular market</div>
+            <div className="shrink-0 text-center text-foreground-light">Most popular</div>
           </CardWithExternalLink>
-          <Card className="lg:col-[5] lg:row-[2]">
+          <Card className="lg:col-[5] lg:row-[2]" delay={0.2}>
             <div className="flex flex-1 flex-col items-center justify-center gap-3 overflow-clip">
               <div className="flex flex-1 flex-row items-center justify-center">
                 <div className="flex flex-row items-center rounded-md bg-white px-2 text-3xl text-black">
