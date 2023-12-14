7734dc157cd1f382015835ba06d55652998b3906
case <case@casesandberg.com>
2023-12-13 23:42:54 -0800

    Refresh for Data with Exponential Backoff

--- components/StartProcessingForUser.tsx
+++ components/StartProcessingForUser.tsx
@@ -3,12 +3,13 @@
 import { User } from '@/lib/manifold'
 import { Stats } from '@/lib/stats'
 import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 export default function StartProcessingForUser({ user, stats }: { user: User; stats: Stats }) {
   const router = useRouter()
   const isEmpty = Object.keys(stats.items).length === 0
   const isGenerating = stats.processing && isEmpty
+  const intervalId = useRef<ReturnType<typeof setTimeout> | null>(null)
 
   useEffect(() => {
     if (isEmpty && !isGenerating) {
@@ -17,16 +18,36 @@ export default function StartProcessingForUser({ user, stats }: { user: User; st
   }, [isEmpty, isGenerating, user.username])
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (!isEmpty) {
-        clearInterval(interval)
-      } else {
-        router.refresh()
-      }
-    }, 15000)
+    let intervalDuration = 1000
+    const maxInterval = 300000
+    const backoffRate = 2
 
-    return () => clearInterval(interval)
-  }, [isEmpty, router])
+    const setNewInterval = () => {
+      if (intervalId.current !== null) {
+        clearInterval(intervalId.current)
+      }
+
+      intervalId.current = setInterval(() => {
+        if (!isEmpty) {
+          if (intervalId.current !== null) {
+            clearInterval(intervalId.current)
+          }
+        } else {
+          router.refresh()
+          intervalDuration = Math.min(intervalDuration * backoffRate, maxInterval)
+          setNewInterval()
+        }
+      }, intervalDuration)
+    }
+
+    setNewInterval()
+
+    return () => {
+      if (intervalId.current !== null) {
+        clearInterval(intervalId.current)
+      }
+    }
+  }, [isEmpty]) // eslint-disable-line react-hooks/exhaustive-deps
 
   return null
 }
